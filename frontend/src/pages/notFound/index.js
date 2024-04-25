5d5b8970a09bfa1f242ad68236ad768f0590a0b8
RajeevGoyal21202 <rajeev.1149@zenmonk.tech>
2024-04-25 18:50:27 +0530

    not found done

--- frontend/src/pages/notFound/index.js
+++ frontend/src/pages/notFound/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import styles from "./notFound.module.css"
-import logo from "../../assets/Logo.png"
+import notlogo from "../../assets/notlogo.svg"
 import notfound from "../../assets/notfound.svg"
-
+import {  useNavigate } from "react-router-dom"
+import { Button } from "@mui/material";
+import AuthFooter from '../../component/AuthFooter'
 
 
 const NotFound = () => {
+ const navigate = useNavigate()
+  const handleClick = ()=>{
+    navigate("/home")
+  }
   return (
+
    <div className={styles.maincontainer}>
     <div className={styles.outerNavbar}>
       <div className={styles.innerNavbar}>
-        <img src={logo} className={styles.logo}></img>
+        <img src={notlogo} className={styles.logo}></img>
       </div>
     </div>
     <div className={styles.body}>
@@ -19,13 +26,20 @@ const NotFound = () => {
       <img src={notfound} className={styles.notfound}></img>
         </div>
       <div className={styles.text}>
-        <h2 className={styles.heading}>The page doesn't exist</h2>
-        <p></p>
+        <h2 className={styles.heading}>This page doesn't exist</h2>
+        <p className={styles.para}>Please check your URL or return to LinkedIn home</p>
+        <div className={styles.button}>
+
+        <Button variant='outlined' onClick={handleClick} className={styles.feedButton}>Go to your feed</Button>
+        </div>
+        
       </div>
       </div>
       
     </div>
-
+    <div className={styles.footer}>
+    </div>
+    <AuthFooter/>
    </div>
   )
 }
